8a2cefde8ad913f95ea7b02521f4e808cb3c3ea9
Ubuntu <azureuser@reportvm.s2tmix2zqzoelcnsjlpisejv0d.gvxx.internal.cloudapp.net>
2025-11-05 09:49:10 +0000

    dailyreport.csv to blob storage

--- src/main.py
+++ src/main.py
@@ -1,10 +1,16 @@
 from report_queries import Queries
-
-
+from datetime import datetime, timedelta
 
 query = Queries()
+today = datetime.today()
+today_date = today.strftime('%Y-%m-%d')
+start_date = today - timedelta(days = 7)
+start_date = start_date.strftime('%Y-%m-%d')
+print(today_date, start_date)
 
-df = query.get_daily_report('2025-11-02',  '2025-11-06')
+
+df = query.get_daily_report(start_date, today_date)
 print(df)
 
 query.close_connections()
+df.to_csv('dailyreport.csv', index = False)
--- src/report_sdk.py
+++ src/report_sdk.py
@@ -1,30 +1,30 @@
 import os
 from azure.identity import DefaultAzureCredential
 from azure.storage.blob import BlobServiceClient
-# Replace <storage-account-name> with your actual storage account name
+
 account_url = "https://timeman.blob.core.windows.net"
 credential = DefaultAzureCredential()
 # Create the BlobServiceClient object
-blob_service_client = BlobServiceClient(account_url, credential=credential)
-
+#blob_service_client = BlobServiceClient(account_url, credential=credential)
+blob_service_client = BlobServiceClient.from_connection_string("DefaultEndpointsProtocol=https;AccountName=timeman;AccountKey=174prRRvf77WFNCWTCSezs0cJzoqTPIYXms3DTwjwNbgiEFOBj1B/x2lECVtGMFWMFddM8iq3wk0+AStm+tVDQ==;EndpointSuffix=core.windows.net")
 def upload_blob_file(blob_service_client, container_name):
     container_client = blob_service_client.get_container_client(container=container_name)
     
-    with open(file=os.path.join('C:/Users/IliaZubov/Documents/Skillio/week 3/src/data/', 'something.txt'), mode="rb") as data:
-        blob_client = container_client.upload_blob(name="report-blob.txt", data=data, overwrite=True)
+    with open(file=os.path.join('/home/azureuser/TimeManagement/src', 'dailyreport.csv'), mode="rb") as data:
+        blob_client = container_client.upload_blob(name="dailyreport.csv", data=data, overwrite=True)
         
-#upload_blob_file(blob_service_client, "report-container")
+upload_blob_file(blob_service_client, "report-container")
 
-container_name = "report-container"
+#container_name = "report-container"
 
-blob_name = "report-blob.txt"
+#blob_name = "report-blob.txt"
 
-download_path = "C:/Users/IliaZubov/Downloads/report-blob.txt"
+#download_path = "C:/Users/IliaZubov/Downloads/report-blob.txt"
 
 # Initialize client
-blob_service_client = BlobServiceClient(account_url, credential=credential)
-blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+#blob_service_client = BlobServiceClient(account_url, credential=credential)
+#blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
 
 # Download blob
-with open(download_path, "wb") as file:
-    file.write(blob_client.download_blob().readall())
+#with open(download_path, "wb") as file:
+   # file.write(blob_client.download_blob().readall())
