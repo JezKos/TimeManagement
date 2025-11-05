06213d1ae792c7b8721735b13b1c49696195ebc8
Ubuntu <azureuser@reportvm.s2tmix2zqzoelcnsjlpisejv0d.gvxx.internal.cloudapp.net>
2025-11-05 11:27:29 +0000

    .csv file timestamp added

--- src/report_sdk.py
+++ src/report_sdk.py
@@ -1,17 +1,19 @@
 import os
 from azure.identity import DefaultAzureCredential
 from azure.storage.blob import BlobServiceClient
+from datetime import datetime
 
 account_url = "https://timeman.blob.core.windows.net"
 credential = DefaultAzureCredential()
 # Create the BlobServiceClient object
-#blob_service_client = BlobServiceClient(account_url, credential=credential)
-blob_service_client = BlobServiceClient.from_connection_string("DefaultEndpointsProtocol=https;AccountName=timeman;AccountKey=174prRRvf77WFNCWTCSezs0cJzoqTPIYXms3DTwjwNbgiEFOBj1B/x2lECVtGMFWMFddM8iq3wk0+AStm+tVDQ==;EndpointSuffix=core.windows.net")
+blob_service_client = BlobServiceClient(account_url, credential=credential)
+#blob_service_client = BlobServiceClient.from_connection_string("DefaultEndpointsProtocol=https;AccountName=timeman;AccountKey=174prRRvf77WFNCWTCSezs0cJzoqTPIYXms3DTwjwNbgiEFOBj1B/x2lECVtGMFWMFddM8iq3wk0+AStm+tVDQ==;EndpointSuffix=core.windows.net")
+today_stamp = datetime.today()
 def upload_blob_file(blob_service_client, container_name):
     container_client = blob_service_client.get_container_client(container=container_name)
     
     with open(file=os.path.join('/home/azureuser/TimeManagement/src', 'dailyreport.csv'), mode="rb") as data:
-        blob_client = container_client.upload_blob(name="dailyreport.csv", data=data, overwrite=True)
+        blob_client = container_client.upload_blob(name=f"dailyreport {today_stamp}.csv", data=data, overwrite=True)
         
 upload_blob_file(blob_service_client, "report-container")
 
